ecefdde0f9f3baa14c8d508222a2a12f2da7310b
Grant Carroll <gcarroll2603@gmail.com>
2014-10-27 01:55:07 -0400

    Added comments to outline

--- polynomial.cpp
+++ polynomial.cpp
@@ -4,16 +4,34 @@
 
 using namespace std;
 
-//Getters
-int Polynomial::getCoefficientAt(int power) const{}
-int Polynomial::operator[](int power) const{}
+//Getter
+int Polynomial::getCoefficientAt(int power) const
+{
+    return coefficients.at(power);
+}
 
-//Arithmetic Operators
+//Addition Operator
+    //Will add the coefficients of the polynomials together and return the resulting polynomial
+    //ex. (ax^n + bx^n-1 + ... + cx^1 + d) + (ex^n + fx^n-1 + ... + gx^1 + h)
+    //      = (a+e)x^n + (b+f)x^n-1 + ... + (c+g)x^1 + (d+h)
 Polynomial Polynomial::operator+(const Polynomial& a){}
+
+//Subtraction Operator
+    //Will subtract the coefficients of the polynomials and return the resulting polynomial
+    //ex. (ax^n + bx^n-1 + ... + cx^1 + d) - (ex^n + fx^n-1 + ... + gx^1 + h)
+    //      = (a-e)x^n + (b-f)x^n-1 + ... + (c-g)x^1 + (d-h)
 Polynomial Polynomial::operator-(const Polynomial& a){}
+
+//Multiplication Operator
+    //Not in the original design but simple enough to implement
+    //Multiplies each coefficient by the given int
+    //ex. 5 * (ax^n + bx^n-1 + ... + cx^1 + d) = 5ax^n + 5bx^n-1 + ... + 5cx^1 + 5d
+Polynomial Polynomial::operator*(int a){}
+
 void Polynomial::operator=(const Polynomial& a){}
 void Polynomial::operator+=(const Polynomial& a){}
 void Polynomial::operator-=(const Polynomial& a){}
+void Polynomial::operator*=(int a){}
 
 //Comparison Operators
 bool operator==(const Polynomial& a, const Polynomial& b){}
--- polynomial.hpp
+++ polynomial.hpp
@@ -10,23 +10,37 @@ class Polynomial
         std::vector<int> coefficients;
     
     public:
+        //Constructors
+            //Can be intitialized with an array or vector of integers representing the coefficients
+            //Can also be initialized using a copy constructor and another Polynomial
         Polynomial(int coefficientArray[], int size) : coefficients(&coefficientArray[0], &coefficientArray[0]+size){}
         Polynomial(std::vector<int> coefficientVector) : coefficients(coefficientVector){}
         Polynomial(const Polynomial& a) : coefficients(a.coefficients){}
         
+        //Getter
+            //Takes in the degree and returns the coefficient at that degree
+            //Vector bounds errors past the bounds of the current polynomial
         int getCoefficientAt(int power) const;
-        int operator[](int power) const;
         
+        //Arithmetic and Assignment Operators
+            //Based on algebraic definitions of operations with polynomials
+            //More detailed explanations are provided with the implementation of each operator
         Polynomial operator+(const Polynomial& a);
         Polynomial operator-(const Polynomial& a);
+        Polynomial operator*(int a);
         void operator=(const Polynomial& a);
         void operator+=(const Polynomial& a);
         void operator-=(const Polynomial& a);
+        void operator*=(int a);
 };
 
+//Comparison Operators
+    //Equality will be defined as having the same maximum degree and all coefficients being equal
 bool operator==(const Polynomial& a, const Polynomial& b);
 bool operator!=(const Polynomial& a, const Polynomial& b);
 
+//Stream Operator
+    //Outputs in the format: ax^n + bx^n-1 + ... + cx^1 + d
 std::ostream& operator<<(std::ostream& os, const Polynomial& t);
 
 #endif
